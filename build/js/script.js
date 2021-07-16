75290ec4b7209da21aece6a2ca6e16068fa00064
Konstantin Bakharev <konstanz.satyr@gmail.com>
2021-07-16 17:57:48 +0300

    исправление багов 2

--- build/js/script.js
+++ build/js/script.js
@@ -88,39 +88,85 @@ function removeModal() {
 
 })();
 
-window.addEventListener("DOMContentLoaded", function() {
-  [].forEach.call( document.querySelectorAll('.tel'), function(input) {
-  var keyCode;
-  function mask(event) {
-      event.keyCode && (keyCode = event.keyCode);
-      var pos = this.selectionStart;
-      if (pos < 3) event.preventDefault();
-      var matrix = "+7 (___) ___ ____",
-          i = 0,
-          def = matrix.replace(/\D/g, ""),
-          val = this.value.replace(/\D/g, ""),
-          new_value = matrix.replace(/[_\d]/g, function(a) {
-              return i < val.length ? val.charAt(i++) || def.charAt(i) : a
-          });
-      i = new_value.indexOf("_");
-      if (i != -1) {
-          i < 5 && (i = 3);
-          new_value = new_value.slice(0, i)
-      }
-      var reg = matrix.substr(0, this.value.length).replace(/_+/g,
-          function(a) {
-              return "\\d{1," + a.length + "}"
-          }).replace(/[+()]/g, "\\$&");
-      reg = new RegExp("^" + reg + "$");
-      if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
-      if (event.type == "blur" && this.value.length < 5)  this.value = ""
+window.addEventListener('DOMContentLoaded', function() {
+
+  var inputs = document.querySelectorAll('input[type="tel"]');
+
+  Array.prototype.forEach.call(inputs, function(input) {
+    new InputMask({
+      selector: input,
+      layout: input.dataset.mask
+    })
+  })
+
+})
+
+function InputMask(options) {
+  this.el = this.getElement(options.selector);
+  if (!this.el) return console.log('Что-то не так с селектором');
+  this.layout = options.layout || '+7 (___) ___-__-__';
+  this.maskreg = this.getRegexp();
+  this.setListeners();
+}
+
+InputMask.prototype.getRegexp = function () {
+  var str = this.layout.replace(/_/g, '\\d');
+  str = str.replace(/\(/g, '\\(');
+  str = str.replace(/\)/g, '\\)');
+  str = str.replace(/\+/g, '\\+');
+  str = str.replace(/\s/g, '\\s');
+  return str;
+};
+
+InputMask.prototype.mask = function (e) {
+  var _this = e.target,
+      matrix = this.layout,
+      i = 0,
+      def = matrix.replace(/\D/g, ""),
+      val = _this.value.replace(/\D/g, "");
+
+  if (def.length >= val.length) val = def;
+  _this.value = matrix.replace(/./g, function (a) {
+    return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a;
+  });
+
+  if (e.type == "blur") {
+    var regexp = new RegExp(this.maskreg);
+    if (!regexp.test(_this.value)) _this.value = "";
+  } else {
+    this.setCursorPosition(_this.value.length, _this);
+  }
+};
+
+InputMask.prototype.setCursorPosition = function (pos, elem) {
+  elem.focus();
+  if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);else if (elem.createTextRange) {
+    var range = elem.createTextRange();
+    range.collapse(true);
+    range.moveEnd("character", pos);
+    range.moveStart("character", pos);
+    range.select();
+  }
+};
+
+InputMask.prototype.setListeners = function () {
+  this.el.addEventListener("input", this.mask.bind(this), false);
+  this.el.addEventListener("focus", this.mask.bind(this), false);
+  this.el.addEventListener("blur", this.mask.bind(this), false);
+};
+
+InputMask.prototype.getElement = function (selector) {
+  if (selector === undefined) return false;
+  if (this.isElement(selector)) return selector;
+
+  if (typeof selector == 'string') {
+    var el = document.querySelector(selector);
+    if (this.isElement(el)) return el;
   }
 
-  input.addEventListener("input", mask, false);
-  input.addEventListener("focus", mask, false);
-  input.addEventListener("blur", mask, false);
-  input.addEventListener("keydown", mask, false)
+  return false;
+};
 
-});
-
-});
+InputMask.prototype.isElement = function (element) {
+  return element instanceof Element || element instanceof HTMLDocument;
+};
